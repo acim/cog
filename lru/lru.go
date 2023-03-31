64c9e73f712558c433e8fbf1e1116332583c11c9
Boban Acimovic <boban.acimovic@gmail.com>
2023-03-31 17:08:43 +0200

    use swiss map

--- lru/lru.go
+++ lru/lru.go
@@ -2,19 +2,21 @@ package lru
 
 import (
 	"sync"
+
+	"github.com/dolthub/swiss"
 )
 
 type Cache[K comparable, V any] struct {
 	size  uint
 	mu    sync.RWMutex
-	inner map[K]*el[K, V]
+	inner *swiss.Map[K, *el[K, V]]
 	lru   *list[K, V]
 }
 
 func NewCache[K comparable, V any](size uint) *Cache[K, V] {
 	return &Cache[K, V]{
 		size:  size,
-		inner: make(map[K]*el[K, V], size),
+		inner: swiss.NewMap[K, *el[K, V]](uint32(size)),
 		lru:   newList[K, V](),
 	}
 }
@@ -22,10 +24,10 @@ func NewCache[K comparable, V any](size uint) *Cache[K, V] {
 func (c *Cache[K, V]) Set(key K, value V) {
 	c.mu.Lock()
 
-	if el, ok := c.inner[key]; ok {
+	if el, ok := c.inner.Get(key); ok {
 		c.lru.toTop(el)
 		el.value = value
-		c.inner[key] = el
+		c.inner.Put(key, el)
 		c.mu.Unlock()
 
 		return
@@ -33,19 +35,19 @@ func (c *Cache[K, V]) Set(key K, value V) {
 
 	if c.lru.size == c.size {
 		el := c.lru.last()
-		delete(c.inner, el.key)
+		c.inner.Delete(el.key)
 		c.lru.remove(el)
 	}
 
 	el := c.lru.prepend(key, value)
-	c.inner[key] = el
+	c.inner.Put(key, el)
 	c.mu.Unlock()
 }
 
 func (c *Cache[K, V]) Get(key K) (V, bool) { //nolint:ireturn
 	c.mu.RLock()
 
-	if el, ok := c.inner[key]; ok {
+	if el, ok := c.inner.Get(key); ok {
 		c.lru.toTop(el)
 		c.mu.RUnlock()
 
